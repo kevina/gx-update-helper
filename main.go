c90c1f9b1f4a85b858a8bc3face4a05d07a47239
Kevin Atkinson <k@kevina.org>
2018-09-04 01:13:50 -0400

    Initial working version.
    Needs more checks.

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func RootPath(path string) (string, error) {
 	if !os.SameFile(curDir, rootDir) {
 		return "", fmt.Errorf("current directory not the projects root directory")
 	}
-	return path, nil
+	return rootPath, nil
 }
 
 func mainFun() error {
@@ -40,50 +40,118 @@ func mainFun() error {
 	if err != nil {
 		return err
 	}
-	if len(os.Args) != 3 {
-		return fmt.Errorf("usage: %s rev-deps|rev-deps-json|rev-deps-list <name>", os.Args[0])
-	}
-	pkgs, todoList, err := Gather(os.Args[2])
-	if err != nil {
-		return err
+	if len(os.Args) < 2 {
+		return fmt.Errorf("usage: %s rev-deps|init|next|published| ...", os.Args[0])
 	}
 	switch os.Args[1] {
 	case "rev-deps":
+		if len(os.Args) != 3 {
+			return fmt.Errorf("usage: %s rev-deps <name>", os.Args[0])
+		}
+		_, todoList, err := Gather(os.Args[2])
+		if err != nil {
+			return err
+		}
 		level := 0
 		for _, todo := range todoList {
 			if level != todo.Level {
 				fmt.Printf("\n")
 				level++
 			}
-			fmt.Printf("%s :: %s\n", todo.Name, strings.Join(todo.Deps, " "))
+			fmt.Printf("%s :: %s\n", todo.Path, strings.Join(todo.Deps, " "))
 		}
 	case "rev-deps-json":
-		return todoList.Write(os.Stdout)
-	case "rev-deps-list":
+		if len(os.Args) != 3 {
+			return fmt.Errorf("usage: %s rev-deps-json <name>", os.Args[0])
+		}
+		_, todoList, err := Gather(os.Args[2])
+		if err != nil {
+			return err
+		}
+		return todoList.ToJSON(os.Stdout)
+	case "rev-deps-list", "bubble-list":
+		if len(os.Args) != 3 {
+			return fmt.Errorf("usage: %s rev-deps-list <name>", os.Args[0])
+		}
+		_, todoList, err := Gather(os.Args[2])
+		if err != nil {
+			return err
+		}
 		for _, todo := range todoList {
 			fmt.Printf("%s\n", todo.Path)
 		}
 	case "init":
+		if len(os.Args) != 3 {
+			return fmt.Errorf("usage: %s init <name>", os.Args[0])
+		}
+		pkgs, todoList, err := Gather(os.Args[2])
+		if err != nil {
+			return err
+		}
+		// Make sure there are no duplicate entries
+		_, err = todoList.CreateMap()
+		if err != nil {
+			return err
+		}
 		rootPath, err := RootPath(pkgs[""].Path)
 		if err != nil {
 			return err
 		}
 		path := filepath.Join(rootPath, ".mygx-workspace.json")
-		todoList.WriteToFile(path)
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("MYGX_WORKSPACE=%s\n", path)
+		defer f.Close()
+		err = todoList.ToJSON(f)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("export MYGX_WORKSPACE=%s\n", path)
+	case "status":
+		todoList, err := ReadTodo()
+		if err != nil {
+			return err
+		}
+		todoByName, err := todoList.CreateMap()
+		if err != nil {
+			return err
+		}
+		level := 0
+		for _, v := range todoList {
+			if level != v.Level {
+				fmt.Printf("\n")
+				level++
+			}
+			if v.NewHash != "" {
+				fmt.Printf("%s = %s\n", v.Path, v.NewHash)
+				continue
+			}
+			deps := []string{}
+			for _, dep := range v.Deps {
+				if todoByName[dep].NewHash == "" {
+					deps = append(deps, dep)
+				}
+			}
+			if len(deps) == 0 {
+				fmt.Printf("%s READY\n", v.Path)
+				continue
+			}
+			fmt.Printf("%s :: %s\n", v.Path, strings.Join(deps, " "))
+		}
 	case "next":
 		todoList, err := ReadTodo()
 		if err != nil {
 			return err
 		}
-		todoByName := map[string]*Todo{}
-		for _, v := range todoList {
-			todoByName[v.Name] = v
+		todoByName, err := todoList.CreateMap()
+		if err != nil {
+			return err
 		}
 		for _, v := range todoList {
+			if v.NewHash != "" {
+				continue
+			}
 			ok := true
 			for _, dep := range v.Deps {
 				if todoByName[dep].NewHash == "" {
@@ -93,6 +161,89 @@ func mainFun() error {
 			if ok {
 				fmt.Printf("%s\n", v.Path)
 			}
+		}
+	case "sync":
+		todoList, err := ReadTodo()
+		if err != nil {
+			return err
+		}
+		todoByName, err := todoList.CreateMap()
+		if err != nil {
+			return err
+		}
+		pkg, err := ReadPackage(".")
+		if err != nil {
+			return err
+		}
+		lastPubVer, err := ReadLastPubVer(".")
+		if err != nil {
+			return err
+		}
+		v, ok := todoByName[pkg.Name]
+		if !ok {
+			return fmt.Errorf("could not find entry for %s", pkg.Name)
+		}
+		if v.NewHash == lastPubVer.Hash {
+			fmt.Fprintf(os.Stderr, "nothing to change: already marked as published with hash %s\n", v.NewHash)
+			return nil
+		} else if v.OrigHash == lastPubVer.Hash {
+			if v.NewHash == "" {
+				fmt.Fprintf(os.Stderr, "nothing to change: not yet published with new hash %s\n", v.NewHash)
+				return nil
+			} else {
+				v.NewHash = ""
+				fmt.Fprintf(os.Stderr, "resetting state to unpublished\n")
+			}
+		} else {
+			if v.NewHash != "" {
+				fmt.Fprintf(os.Stderr, "warning: previous published using hash %s\n", v.NewHash)
+			}
+			v.NewHash = lastPubVer.Hash
+			fmt.Fprintf(os.Stderr, "marking state as published with hash %s\n", v.NewHash)
+		}
+		err = todoList.Write()
+		if err != nil {
+			return err
+		}
+	case "update-cmds":
+		todoList, err := ReadTodo()
+		if err != nil {
+			return err
+		}
+		todoByName, err := todoList.CreateMap()
+		if err != nil {
+			return err
+		}
+		pkg, err := ReadPackage(".")
+		if err != nil {
+			return err
+		}
+		todo, ok := todoByName[pkg.Name]
+		if !ok {
+			return fmt.Errorf("could not find entry for %s", pkg.Name)
+		}
+		cmds := []string{}
+		toUpdate := []string{}
+		for _, dep := range todo.Deps {
+			newHash := todoByName[dep].NewHash
+			if newHash == "" {
+				toUpdate = append(toUpdate, dep)
+			} else {
+				cmds = append(cmds, fmt.Sprintf("gx update %s %s\n", dep, newHash))
+			}
+		}
+		if len(toUpdate) > 0 {
+			return fmt.Errorf("not yet updated: %s", strings.Join(toUpdate, " "))
+		}
+		for _, dep := range todo.AlsoUpdate {
+			newHash := todoByName[dep].NewHash
+			if newHash == "" {
+				panic("inconsistent internal state")
+			}
+			cmds = append(cmds, fmt.Sprintf("gx update %s %s\n", dep, newHash))
+		}
+		for _, cmd := range cmds {
+			os.Stdout.WriteString(cmd)
 		}
 	default:
 		return fmt.Errorf("unknown command: %s", os.Args[1])
--- todo.go
+++ todo.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"sort"
-	"io"
 	//"strings"
 )
 
@@ -23,6 +23,7 @@ type Todo struct {
 }
 
 type TodoList []*Todo
+type TodoByName map[string]*Todo
 
 func (x *Todo) Less(y *Todo) bool {
 	if x.Level != y.Level {
@@ -39,19 +40,20 @@ func (x *Todo) Less(y *Todo) bool {
 	return x.Name < y.Name
 }
 
-func Gather(pkgName string) (Packages, TodoList, error) {
-	pkgs := Packages{}
-	_, err := GatherDeps(pkgs, "", ".")
+func Gather(pkgName string) (pkgs Packages, todoList TodoList, err error) {
+	pkgs = Packages{}
+	_, err = GatherDeps(pkgs, "", ".")
 	if err != nil {
-		return nil, nil, fmt.Errorf("could not gather deps: %s", err.Error())
+		err = fmt.Errorf("could not gather deps: %s", err.Error())
+		return
 	}
 	//pkgs.Dump()
 	target := pkgs.ByName(pkgName)
 	if target == nil {
-		return nil, nil, fmt.Errorf("package not found: %s", os.Args[2])
+		err = fmt.Errorf("package not found: %s", os.Args[2])
+		return
 	}
 	lst := BubbleList(pkgs, target.Hash)
-	todoList := TodoList{}
 	for _, dep := range lst {
 		todoList = append(todoList, &Todo{
 			Name:       pkgs[dep.Hash].Name,
@@ -64,37 +66,53 @@ func Gather(pkgName string) (Packages, TodoList, error) {
 		})
 	}
 	sort.Slice(todoList, func(i, j int) bool { return todoList[i].Less(todoList[j]) })
-	return pkgs, todoList, nil
+	return
 }
 
-func ReadTodo() (TodoList, error) {
-	todoFile := os.Getenv("MYGX_WORKSPACE")
-	if todoFile == "" {
-		return nil, fmt.Errorf("GOPATH not set")
+func ReadTodo() (lst TodoList, err error) {
+	fn := os.Getenv("MYGX_WORKSPACE")
+	if fn == "" {
+		err = fmt.Errorf("MYGX_WORKSPACE not set")
+		return
 	}
-	bytes, err := ioutil.ReadFile(todoFile)
+	bytes, err := ioutil.ReadFile(fn)
 	if err != nil {
-		return nil, err
+		return
 	}
-	todo := &[]*Todo{}
-	err = json.Unmarshal(bytes, todo)
-	if err != nil {
-		return nil, err
-	}
-	return *todo, nil
+	err = json.Unmarshal(bytes, &lst)
+	return
 }
 
-func (todoList TodoList) Write(out io.Writer) error {
+func (todoList TodoList) ToJSON(out io.Writer) error {
 	encoder := json.NewEncoder(out)
 	encoder.SetIndent("", "  ")
 	return encoder.Encode(todoList)
 }
 
-func (todoList TodoList) WriteToFile (path string) error {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0666)
-	defer f.Close()
+// Write writes the contents back to disk, file must already exist as
+// a safety mechanism
+func (todoList TodoList) Write() error {
+	fn := os.Getenv("MYGX_WORKSPACE")
+	if fn == "" {
+		return fmt.Errorf("MYGX_WORKSPACE not set")
+	}
+	f, err := os.OpenFile(fn, os.O_WRONLY|os.O_TRUNC, 0666)
 	if err != nil {
 		return err
 	}
-	return todoList.Write(f)
+	defer f.Close()
+	return todoList.ToJSON(f)
+}
+
+func (todoList TodoList) CreateMap() (TodoByName, error) {
+	byName := TodoByName{}
+	for _, v := range todoList {
+		prev, ok := byName[v.Name]
+		if ok {
+			// FIXME: Maybe be a little more permissive...
+			return nil, fmt.Errorf("duplicate entries for %s: %s and %s", v.Name, prev.OrigHash, prev.OrigHash)
+		}
+		byName[v.Name] = v
+	}
+	return byName, nil
 }
